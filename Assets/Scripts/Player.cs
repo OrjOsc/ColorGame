008e3bf1641eb3a45b015db4f8db5099be3c2de8
OrjOsc <orjosc2005@gmail.com>
2026-03-14 21:34:59 +0100

    Comments & level design

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -3,15 +3,15 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     public float moveSpeed = 5f;    // "f" stands for float, only used for float variables
-    public float jumpForce = 10f;
+    public float jumpForce = 10f;   // "public" = anything can see it, even external libraries
     public Transform groundCheck;
     public float groundCheckRadius = 0.2f;
     public LayerMask groundLayer;
 
     private Rigidbody2D rb;         // "rb" stands for RigidBody
-    private bool isGrounded;        // bool = true or false ; In this case, is the player grounded or not?
+    private bool isGrounded;        // bool = true or false ; In this case. 3Is the player grounded or not?3
 
-    private Animator animator;
+    private Animator animator;      // "private" = only visible within the current code unit
 
     public int extraJumpsValue = 1;
     private int extraJumps;
@@ -27,19 +27,19 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float moveInput = Input.GetAxis("Horizontal");
-        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y); // "Vector2" means it's  a 2D direction
+        float moveInput = Input.GetAxis("Horizontal");  // "Horizontal" = left & right arrows (also A & D) ; "Vertical" = up & down arrows (also W & S)
+        rb.linearVelocity = new Vector2(moveInput * moveSpeed, rb.linearVelocity.y);    // "Vector2" = 2D direction
 
         if(isGrounded)
         {
             extraJumps = extraJumpsValue;
         }
 
-        if(Input.GetKeyDown(KeyCode.Space))
+        if(Input.GetKeyDown(KeyCode.Space))     // "GetKeyDown(...)" = Keyboard key pressed
         {
             if(isGrounded)
             {
-                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce);
+                rb.linearVelocity = new Vector2(rb.linearVelocity.x, jumpForce); // NEW Vector2(), don't forget!
             }
             else if(extraJumps > 0)
             {
@@ -71,7 +71,7 @@ public class Player : MonoBehaviour
         }
         else
         {
-            if(rb.linearVelocityY > 0)
+            if(rb.linearVelocityY > 0)  // 
             {
                 animator.Play("Player_Jump");
             }
